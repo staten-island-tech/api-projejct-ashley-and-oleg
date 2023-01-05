62d79bc9f879333e40aa7ba53ba104cc4c7b39ac
ashleyl385 <ashleyliwei607@gmail.com>
2023-01-05 10:24:25 -0500

    1/5/23

--- app/flaskr/__init__.py
+++ app/flaskr/__init__.py
@@ -4,11 +4,12 @@ import json
 
 # r = requests.get("https://api.bestbuy.com/v1/products/trendingViewed(categoryId=abcat0400000)?apiKey=tyP7wRXV364rYiZc16B7dJAt").json()
 # print(r)
-
+# r = requests.get("https://api.bestbuy.com/v1/products/8880044/alsoViewed?apiKey=tyP7wRXV364rYiZc16B7dJAt").json()
+# print(r)
 data = requests.get("https://api.bestbuy.com/v1/products/trendingViewed(categoryId=abcat0400000)?apiKey=tyP7wRXV364rYiZc16B7dJAt").json()
-
+data2 = requests.get("https://api.bestbuy.com/v1/products/8880044/alsoViewed?apiKey=tyP7wRXV364rYiZc16B7dJAt").json()
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template('homepage.html', data=data)
+    return render_template('homepage.html', data=data, data2=data2)
