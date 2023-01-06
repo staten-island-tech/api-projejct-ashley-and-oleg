4872fe3d4dc8edc7de82b06639ef951b66c8bd2a
ashleyl385 <ashleyliwei607@gmail.com>
2023-01-06 11:05:08 -0500

    1/6/23

--- app/flaskr/__init__.py
+++ app/flaskr/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
 import json
 
@@ -9,3 +9,14 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template('mainpage.html', data=data, data2=data2)
+
+@app.route('/', methods=('GET', 'POST'))
+def getPost():
+    if request.method == 'POST':
+        title = request.form['title']
+        data = requests.get(f"https://api.bestbuy.com/v1/products/mostViewed(categoryId=abcat0107000)?format=json&show=name,salePrice,description,image&apiKey=tyP7wRXV364rYiZc16B7dJAt{}").json()
+
+        print(request.form)
+        return render_template('test.html',data=data)
+    else:
+        return render_template('mainpage.html')
