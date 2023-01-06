2d0abc0dda96b8203d27f82089ca9d526af91915
olegponyat <113192651+olegponyat@users.noreply.github.com>
2023-01-06 10:42:00 -0500

    desa

--- app/flaskr/__init__.py
+++ app/flaskr/__init__.py
@@ -2,14 +2,10 @@ from flask import Flask, render_template
 import requests
 import json
 
-# r = requests.get("https://api.bestbuy.com/v1/products/trendingViewed(categoryId=abcat0400000)?apiKey=tyP7wRXV364rYiZc16B7dJAt").json()
-# print(r)
-# r = requests.get("https://api.bestbuy.com/v1/products/8880044/alsoViewed?apiKey=tyP7wRXV364rYiZc16B7dJAt").json()
-# print(r)
-data = requests.get("https://api.bestbuy.com/v1/products/trendingViewed(categoryId=abcat0400000)?apiKey=tyP7wRXV364rYiZc16B7dJAt").json()
+data = requests.get("https://api.bestbuy.com/v1/products/mostViewed(categoryId=abcat0107000)?format=json&show=name,salePrice,description,image&apiKey=tyP7wRXV364rYiZc16B7dJAt").json()
 data2 = requests.get("https://api.bestbuy.com/v1/products/8880044/alsoViewed?apiKey=tyP7wRXV364rYiZc16B7dJAt").json()
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template('homepage.html', data=data, data2=data2)
+    return render_template('mainpage.html', data=data, data2=data2)
