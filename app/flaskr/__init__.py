17b4abbea71ace636f5d4c8ffab3ee9109458f01
olegponyat <113192651+olegponyat@users.noreply.github.com>
2023-01-06 11:06:11 -0500

    cf

--- app/flaskr/__init__.py
+++ app/flaskr/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 import requests
 import json
 
-data = requests.get("https://api.bestbuy.com/v1/products/mostViewed(categoryId=abcat0107000)?format=json&show=name,salePrice,description,image&apiKey=tyP7wRXV364rYiZc16B7dJAt").json()
+data = requests.get("https://api.bestbuy.com/v1/products/trendingViewed(categoryId=abcat0400000)?format=json&show=name,salePrice,description,image&apiKey=tyP7wRXV364rYiZc16B7dJAt").json()
 data2 = requests.get("https://api.bestbuy.com/v1/products/8880044/alsoViewed?apiKey=tyP7wRXV364rYiZc16B7dJAt").json()
 app = Flask(__name__)
 
