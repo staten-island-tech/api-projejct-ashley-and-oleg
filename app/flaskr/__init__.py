0ce3073248d978ab570b621bbd7ba80ee52de62e
ashleyl385 <ashleyliwei607@gmail.com>
2023-01-09 11:03:47 -0500

    1/9/23

--- app/flaskr/__init__.py
+++ app/flaskr/__init__.py
@@ -13,10 +13,18 @@ def home():
 @app.route('/', methods=('GET', 'POST'))
 def getPost():
     if request.method == 'POST':
-        title = request.form['title']
-        data = requests.get(f"https://api.bestbuy.com/v1/products/mostViewed(categoryId=abcat0107000)?format=json&show=name,salePrice,description,image&apiKey=tyP7wRXV364rYiZc16B7dJAt{}").json()
+        title = request.form['names']
+        data = requests.get(f"https://api.bestbuy.com/v1/products/mostViewed(categoryId=abcat0107000)?format=json&show=name,salePrice,description,image&apiKey=tyP7wRXV364rYiZc16B7dJAt/{title}").json()
 
         print(request.form)
         return render_template('test.html',data=data)
     else:
         return render_template('mainpage.html')
+
+@app.route('/<path>')
+def searchbar(cards):
+    list = []
+    for thing in data or data2:
+        list.append(thing)
+    if cards in list:
+        return render_template('test.html')
