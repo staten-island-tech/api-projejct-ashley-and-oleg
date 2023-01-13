80298109a7cda8450fdda07288904f2495760b63
ashleyl385 <ashleyliwei607@gmail.com>
2023-01-13 11:03:50 -0500

    1/13/23

--- app/flaskr/__init__.py
+++ app/flaskr/__init__.py
@@ -4,7 +4,7 @@ import json
 
 data = requests.get("https://api.bestbuy.com/v1/products/trendingViewed(categoryId=abcat0400000)?format=json&show=name,salePrice,description,image&apiKey=tyP7wRXV364rYiZc16B7dJAt").json()
 
-data2 = requests.get("https://api.bestbuy.com/v1/products/8880044/alsoViewed?format=json&show=name,salePrice,description,image&apiKey=tyP7wRXV364rYiZc16B7dJAt").json()
+data2 = requests.get("https://api.bestbuy.com/v1/products/mostViewed(categoryId=abcat0107000)?format=json&show=name,salePrice,description,image&apiKey=tyP7wRXV364rYiZc16B7dJAt").json()
 
 app = Flask(__name__)
 
@@ -13,8 +13,8 @@ def home():
     return render_template('mainpage.html', data=data, data2=data2)
 
 @app.route("/dropdown/")
-def getMovies():
-    return render_template('dropdown1.html', data2=data2)
+def getStuff():
+    return render_template('dropdown1.html', data=data)
 
 
 @app.route('/', methods=('GET', 'POST'))
