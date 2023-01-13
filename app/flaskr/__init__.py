4585b37dfaeea5c7544c725d45c79c7ba1a8e8c0
ashleyl385 <ashleyliwei607@gmail.com>
2023-01-13 10:53:01 -0500

    1/13/23

--- app/flaskr/__init__.py
+++ app/flaskr/__init__.py
@@ -2,16 +2,24 @@ from flask import Flask, render_template, request
 import requests
 import json
 
-data = requests.get("https://api.bestbuy.com/v1/products/mostViewed(categoryId=abcat0107000)?format=json&show=name,salePrice,description,image&apiKey=tyP7wRXV364rYiZc16B7dJAt").json()
+data = requests.get("https://api.bestbuy.com/v1/products/trendingViewed(categoryId=abcat0400000)?format=json&show=name,salePrice,description,image&apiKey=tyP7wRXV364rYiZc16B7dJAt").json()
+
 data2 = requests.get("https://api.bestbuy.com/v1/products/8880044/alsoViewed?format=json&show=name,salePrice,description,image&apiKey=tyP7wRXV364rYiZc16B7dJAt").json()
+
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template('mainpage.html', data=data, data2=data2)
 
+@app.route("/dropdown/")
+def getMovies():
+    return render_template('dropdown1.html', data2=data2)
+
+
 @app.route('/', methods=('GET', 'POST'))
 def getPost():
+    
     if request.method == 'POST':
         title = request.form['names']
         data = requests.get(f"'https://api.bestbuy.com/v1/products/mostViewed(categoryId=abcat0107000)?format=json&show=name,salePrice,description,image&apiKey=tyP7wRXV364rYiZc16B7dJAt{title}").json()
