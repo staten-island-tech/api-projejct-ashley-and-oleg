66ccc246cfdaf1da9e42fb540176a5bbe8e620c6
olegponyat <113192651+olegponyat@users.noreply.github.com>
2023-01-17 11:47:33 -0500

    eadasd

--- app/flaskr/__init__.py
+++ app/flaskr/__init__.py
@@ -3,13 +3,12 @@ import requests
 import json
 
 data = requests.get("https://api.bestbuy.com/v1/products/trendingViewed(categoryId=abcat0400000)?format=json&show=name,salePrice,description,image&apiKey=tyP7wRXV364rYiZc16B7dJAt").json()
-data2 = requests.get("https://api.bestbuy.com/v1/products/8880044/alsoViewed?apiKey=tyP7wRXV364rYiZc16B7dJAt").json()
+data2 = requests.get("https://api.bestbuy.com/v1/products/mostViewed(categoryId=abcat0107000)?format=json&show=name,salePrice,description,image&apiKey=tyP7wRXV364rYiZc16B7dJAt").json()
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template('mainpage.html', data=data, data2=data2)
-@app.route("/product/<path:product>")
 
 @app.route("/camera/")
 def getStuff():
