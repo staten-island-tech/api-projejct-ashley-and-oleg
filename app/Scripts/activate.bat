99554a791b9832bfa46ea3e31022d96fdd88e3e4
olegponyat <113192651+olegponyat@users.noreply.github.com>
2023-01-18 20:50:11 -0500

    x

--- app/Scripts/activate.bat
+++ app/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\oleg.ponyatyshyn\Documents\GitHub\api-projejct-ashley-and-oleg\app
+set VIRTUAL_ENV=C:\Users\oleg2\Documents\GitHub\api-projejct-ashley-and-oleg\app
 
 if not defined PROMPT set PROMPT=$P$G
 
